f2b2cf9b6f3080ad3751412e27cf9f2ad232e69c
AyHa1810 <63386848+AyHa1810@users.noreply.github.com>
2021-10-11 10:05:18 +0530

    Update fix.sh

--- fix/fix.sh
+++ fix/fix.sh
@@ -5,6 +5,7 @@ mkdir ./temp
 cd ./temp
 sudo apt-get -y build-dep multistrap
 wget -O pkg.tar.xz http://deb.debian.org/debian/pool/main/m/multistrap/multistrap_2.2.11.tar.xz
+tar -xvf pkg.tar.xz
 cd multistrap-2.2.11
 patch -p1 < ../../fix.patch multistrap
 make install
